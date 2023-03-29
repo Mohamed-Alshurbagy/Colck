126d9a65f4c1088ee58edbccb35e5034a05eaad7
Mohamed Al-Shurbagy <114092198+Mohamed-Alshurbagy@users.noreply.github.com>
2023-03-29 20:17:20 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,42 @@
+
 // Collecting Data From Document:
 
-const hours = document.getElementById("hours");
-const min = document.getElementById("min");
-const seconds = document.getElementById("seconds");
-const bIcon = document.querySelector('.burger-icon');
-const head = document.querySelector('.head')
-const close = document.querySelector('.close');
+const hours = document.querySelector('.hours');
+const min = document.querySelector('.minuts');
+const seconds = document.querySelector('.seconds');
+let bIcon = document.querySelector('.main .burger-icon');
+let head = document.querySelector('.main .head');
+let close = document.querySelector('.main .close');
 
 setInterval(function(){
-  // Creating Time:
+
+  // Creating Time :
   let time = new Date(); // Create Instance From Date Object To 'time' Varable.
-  let h = time.getHours(); // Here We Get Hours Frome Our Main Var (time).
-  let m = time.getMinutes(); // Now We Get Minutes From Same Var.
-  let s = time.getSeconds(); // Getting Our Seconds.
-  // Convertimg To Current Time:
-  let hr = 30 * h + m / 2;
-  let mr = m * 6;
-  let sr = s * 6;
+  let hour = time.getHours(); // Here We Get Hours Frome Our Main Var (time).
+  let minut =  time.getMinutes(); // Now We Get Minutes From Same Var.
+  let second = time.getSeconds(); // Getting Our Seconds.
+
+  // Converting To Current Time:
+  let hr = 30 * hour + minut / 2;
+  let mr = 6 * minut;
+  let sr = 6 * second;
+
   // Add Our Work To Element On HTML & Make it Move:
   hours.style.transform = `rotate(${hr}deg)`;
   min.style.transform = `rotate(${mr}deg)`;
   seconds.style.transform = `rotate(${sr}deg)`;
-}, 1000)
+}, 1000);
 
-bIcon.onclick =function(){
-  head.classList.toggle('s');
+
+// Controle Our Close & Burger Icon (Show-1 & Hide-2):
+bIcon.onclick = function(){ // 1
   this.style.display = 'none';
+  head.classList.add("toggle")
 }
 
-close.onclick = function(){
-  this.parentElement.classList.remove('s');
+close.onclick = function(){ // 2
   bIcon.style.display = 'flex';
+  head.classList.remove("toggle")
 }
 
 // End Of The Code ..
